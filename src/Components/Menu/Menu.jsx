268da6e5ccf11eddc006e469acfe4b4e9b6ffe5e
JalenIOS <jalenarms@outlook.com>
2025-01-10 08:44:45 -0800

    committing progress - finished menu display with options for toppings and drinks, need to implement constructing of the cart to prepare for placing order

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -15,31 +15,36 @@ const Menu = () => {
 
         <div className="grid grid-cols-1 m-2 gap-5">
             {MENU_SECTIONS.map(({img, label, description}, i) => {
-                return <MenuLinkCard key={i} img={img} label={label} description={description} />
+                if (label == "Drinks") {
+                    return <Link to={`/menu/drinks`}>
+                        <div className="flex gap-5">
+                            <div className='w-1/3 h-28 rounded-md bg-zinc-400 text-zinc-600 flex justify-center items-center'>
+                                <PiBeerBottle className='text-6xl' />
+                            </div>
+
+                            <div className="flex flex-col justify-between flex-1">
+                                <div className="flex flex-col">
+                                    <p className='text-lg font-semibold'>Drinks</p>
+                                    <p className='text-zinc-300 text-sm'>Choose from a variety of ice-cold beverages</p>
+
+                                </div>
+
+                                <div className="w-full flex justify-end">
+                                    <IoIosArrowForward className='text-zinc-400' />
+
+                                </div>
+                            </div>
+
+                        </div>
+                    
+                    </Link>
+
+                } else {
+                    return <MenuLinkCard key={i} img={img} label={label} description={description} />
+
+                }
 
             })}
-            <Link to={`/menu/drinks`}>
-                <div className="flex gap-5">
-                    <div className='w-1/3 h-28 rounded-md bg-zinc-400 text-zinc-600 flex justify-center items-center'>
-                        <PiBeerBottle className='text-6xl' />
-                    </div>
-
-                    <div className="flex flex-col justify-between flex-1">
-                        <div className="flex flex-col">
-                            <p className='text-lg font-semibold'>Drinks</p>
-                            <p className='text-zinc-300 text-sm'>Choose from a variety of ice-cold beverages</p>
-
-                        </div>
-
-                        <div className="w-full flex justify-end">
-                            <IoIosArrowForward className='text-zinc-400' />
-
-                        </div>
-                    </div>
-
-                </div>
-            
-            </Link>
         </div>
                 
     </div>
